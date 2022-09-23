a3594e51e80a443c31fb14bcf28d359b719a1b1b
Garfield <iju1633@gmail.com>
2022-09-24 00:03:30 +0900

    project start

---
+++ src/main/java/com/sogong/tejava/TejavaApplication.java
@@ -0,0 +1,13 @@
+package com.sogong.tejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TejavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TejavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sogong/tejava/TejavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sogong.tejava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TejavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
